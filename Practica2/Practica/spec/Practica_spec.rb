966890435e896e10bcc3ed795ebb0c2d9041a644
alu0100789331 <alu0100789331@edu.ull.es>
2017-01-16 02:50:46 +0000

    Estructura primaria y primeras pruebas generadas

---
+++ Practica2/Practica/spec/Practica_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+
+describe "Practica" do
+  describe "Comidas" do
+    it "Se puede construir un obejto" do
+      
+    end
+  end
+  
+  describe "Menu" do
+    it "Se puede construir un objeto" do
+      
+    end
+  end
+  
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+  
+end
