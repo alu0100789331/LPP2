1a3eb5e9871c1d1034eb9533936f22369460215f
alu0100789331 <alu0100789331@edu.ull.es>
2017-01-16 02:54:33 +0000

    Mejoras en las pruebas

--- Practica2/Practica/spec/Practica_spec.rb
+++ Practica2/Practica/spec/Practica_spec.rb
@@ -2,15 +2,23 @@ require "spec_helper"
 
 
 describe "Practica" do
+  
+  before(:all) do
+    @plato_prueba = Plato.new()
+    @menu_prueba = Menu.new()
+  end
+  
+  
+  
   describe "Comidas" do
     it "Se puede construir un obejto" do
-      
+      expect(@plato_prueba).to_not be nil
     end
   end
   
   describe "Menu" do
     it "Se puede construir un objeto" do
-      
+      expect(@menu_prueba).to_not be nil
     end
   end
   
