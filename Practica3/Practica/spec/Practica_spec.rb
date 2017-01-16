512cc4c4ca23db409e0ce908a5ccb4ded31e19e0
alu0100789331 <alu0100789331@edu.ull.es>
2017-01-16 12:56:13 +0000

    Lista simplemente enlazada y nodos simples

--- Practica3/Practica/spec/Practica_spec.rb
+++ Practica3/Practica/spec/Practica_spec.rb
@@ -1,2 +1,20 @@
 require "spec_helper"
 
+describe "Practica" do
+  before(:all) do
+    node = Node.new()
+    lista = Lista.new()
+  end
+  
+  describe "Node" do
+    it "Debe de tener un next" do
+      expect(@node.Next_).not_to be nil
+    end
+    
+    it "Debe de tener un Dato" do
+      expect(@node.Data_).not_to be nil
+    end
+  end
+  
+  
+end
