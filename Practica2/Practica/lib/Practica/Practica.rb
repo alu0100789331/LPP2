e88fa0bfa28b16456ae7c18c85aa0b7d83ec95bf
alu0100789331 <alu0100789331@edu.ull.es>
2017-01-16 04:21:49 +0000

    Mucha mierda hecha :)

--- Practica2/Practica/lib/Practica/Practica.rb
+++ Practica2/Practica/lib/Practica/Practica.rb
@@ -1,14 +1,20 @@
 class Plato
-    attr_accessor :Name_, :Slice_, :Vol_
+    attr_accessor :Name_, :Slice_, :Vol_, :Fat_, :Protein_, :Hydrates_
     def initialize(*args)
         if(args.count()==0)
             @Name_="Plato Vacio"
             @Slice_ = 0
             @Vol_ = 0
-        elsif(args.count()==3)
+            @Fat_ = 0
+            @Protein_ = 0
+            @Hydrates_ = 0
+        elsif(args.count()==6)
             @Name_ = args[0]
             @Slice_ = args[1]
             @Vol_ = args[2]
+            @Fat_ = args[3]
+            @Protein_ = args[4]
+            @Hydrates_ = args[5]
         else
            "Error en el constructor"
         end        
@@ -29,14 +35,43 @@ end
 
 
 class Menu
-    attr_accessor
+    attr_accessor :Name_, :Platos_, :Total_protein_, :Total_hydrate_, :Total_fat_, :Total_VCT_
     def initialize(*args)
         if(args.count()==0)
+            @Name_ = "Menu Vacio"
+            @Platos_ = []
             
-        elsif(args.count())
-        
+            @Total_fat_ = 0
+            @Total_hydrate_ = 0
+            @Total_protein_ = 0
+            @Total_VCT_ = 0
+        elsif(args.count()==2)
+            @Name_ = args[0]
+            @Platos_ = args[1]
+            
+            @Total_fat_ = 0.0
+            @Total_hydrate_ = 0.0
+            @Total_protein_ = 0.0
+            @Total_VCT_ = 0.0
+            
+            @Platos_.each do |plato|                                                                         #Recorremos la lista de platos para calcular los valores del menu
+                @Total_fat_ = @Total_fat_ + plato.Fat_                                                       #Calculamos el total de grasa
+                @Total_hydrate_ = @Total_hydrate_ + plato.Hydrates_                                          #   """""""""""""""""""hidratos
+                @Total_protein_ = @Total_protein_ + plato.Protein_                                           #   """""""""""""""""""proteina
+            end
+            
+            @Total_VCT_ = (@Total_fat_ + @Total_hydrate_ + @Total_protein_)                                  #Calculamos el total de todo
+            
+            @Total_fat_ = (@Total_fat_/@Total_VCT_).to_f*100
+            @Total_hydrate_ = (@Total_hydrate_/@Total_VCT_)*100
+            @Total_protein_ = (@Total_protein_/@Total_VCT_)*100
+            
+            puts @Total_fat_
+
         else
            "Error en el constructor"
         end
     end
+    
+    
 end
--- Practica2/Practica/spec/Practica_spec.rb
+++ Practica2/Practica/spec/Practica_spec.rb
@@ -4,7 +4,13 @@ require "spec_helper"
 describe "Practica" do
   
   before(:all) do
-    @Plato = Plato.new("Macarrones con salsa de tomate y queso parmesano","1 1/2 cucharon","200 gr")
+    @Menu = Menu.new()
+    
+    @Plato = Plato.new("Macarrones con salsa de tomate y queso parmesano","1 1/2 cucharon","200 gr",70,20,150)
+    @Plato1 = Plato.new("escalope de ternera","1 bistec mediano","100 gr",70,20,150)
+    @Plato2 = Plato.new("Ensalada basica con zanahoria rallada","guarnion","120 gr",70,20,150)
+    @Plato3 = Plato.new("Mandarina","1 grande","20 gr",70,20,150)
+    @Plato4 = Plato.new("Pan de trigo integral","1 rodaja","20 gr",70,20,150)
   end
   
   describe "Plato" do
@@ -14,8 +20,9 @@ describe "Practica" do
       expect(@aux.Name_).to eq("Plato Vacio")
     end
     it "Se puede construir un objeto con parametros" do
-      @aux = Plato.new("Macarrones con salsa de tomate y queso parmesano","1 1/2 cucharon","200 gr")
+      @aux = Plato.new("Macarrones con salsa de tomate y queso parmesano","1 1/2 cucharon","200 gr",70,20,150)
       expect(@aux).to_not be nil
+      expect(@aux.Name_).to eq("Macarrones con salsa de tomate y queso parmesano")
     end
     it "Se puede obtener el nombre del plato" do
       expect(@Plato.get_name()).to eq("Macarrones con salsa de tomate y queso parmesano")
@@ -29,10 +36,20 @@ describe "Practica" do
   end
   
   describe "Menu" do
-    it "Se puede construir un objeto" do
+    it "Se puede construir un objeto sin parametros" do
       @aux = Menu.new()
       expect(@aux).to_not be nil
+      expect(@aux.Name_).to eq("Menu Vacio")
+    end
+    
+    it "Se puede construir un objeto con parametros" do
+      @aux = Menu.new("Almuerzo",[@Plato,@Plato1,@Plato2,@Plato3,@Plato4])
+      expect(@aux).to_not be nil
+      expect(@aux.Name_).to eq("Almuerzo")
+    end
+    
+    it "Se puede obtener el nombre del Menu" do
+      expect(@Menu.Name_).to eq("Menu Vacio")
     end
   end
-  
 end
